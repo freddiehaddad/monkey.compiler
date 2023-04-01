5c6aa87300f148604b66d5a433a05c7210b124da
Freddie Haddad <6127369+freddiehaddad@users.noreply.github.com>
2023-03-31 19:46:48 -0700

    feat(compiler): initial compiler framework implemented (#2)
    A framework for opcodes and compiling constant integer literal
    expressions is defined along with a suite of tests that cover:
    
        - creating an instruction
        - decoding an instruction
        - compiling constant expressions
        - storing constants on the stack
        - instruction addresses

---
+++ pkg/compiler/compiler_test.go
@@ -0,0 +1,118 @@
+// The Monkey Language compiler definition unit tests
+package compiler
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/freddiehaddad/monkey.compiler/pkg/code"
+	"github.com/freddiehaddad/monkey.interpreter/pkg/ast"
+	"github.com/freddiehaddad/monkey.interpreter/pkg/lexer"
+	"github.com/freddiehaddad/monkey.interpreter/pkg/object"
+	"github.com/freddiehaddad/monkey.interpreter/pkg/parser"
+)
+
+type compilerTestCase struct {
+	input                string
+	expectedConstants    []interface{}
+	expectedInstructions []code.Instructions
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1 + 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+		compiler := New()
+		if err := compiler.Compile(program); err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		bytecode := compiler.Bytecode()
+
+		if err := testInstructions(tt.expectedInstructions,
+			bytecode.Instructions); err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+
+		if err := testConstants(t, tt.expectedConstants, bytecode.Constants); err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testInstructions(expected []code.Instructions, actual code.Instructions) error {
+	concatenated := concatenateInstructions(expected)
+
+	if len(actual) != len(concatenated) {
+		return fmt.Errorf("wrong instructions length.\nwant=%q\ngot=%q", concatenated, actual)
+	}
+
+	for i, ins := range concatenated {
+		if actual[i] != ins {
+			return fmt.Errorf("wrong instruction at %d.\nwant=%q\ngot=%q", i, concatenated, actual)
+		}
+	}
+
+	return nil
+}
+
+func concatenateInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+
+	return out
+}
+
+func testConstants(t *testing.T, expected []interface{}, actual []object.Object) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants.\ngot=%d\nwant=%d", len(actual), len(expected))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			if err := testIntegerObject(int64(constant), actual[i]); err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+
+	return nil
+}
